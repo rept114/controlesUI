5667bc0aedf415a4aa805034b48103a7a6cfed98
Fer <reikafer@gmail.com>
2022-08-12 07:24:30 -0700

    Initial Commit

---
+++ controlUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  controlUI
+//
+//  Created by Alumno on 8/12/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
